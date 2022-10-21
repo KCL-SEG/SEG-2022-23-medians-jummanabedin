0de4a268ccfdaeb9f1a24c020cc3d6c6f47c2acd
jummanabedin <91836600+jummanabedin@users.noreply.github.com>
2022-10-21 18:04:22 +0100

    return median of list

--- median.py
+++ median.py
@@ -1,6 +1,7 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +10,11 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+
+numbers.sort()
+median = 0
+if len(numbers) % 2 == 0:
+    median = (numbers[len(numbers) - 1] // 2 + numbers[len(numbers)] // 2)/2
+else:
+    median = numbers[len(numbers) - 1] // 2
+print(median)
